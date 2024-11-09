44a46d4f7bb30bd4afd60369dd36d374f78eba2d
rana badran <rana.albadran@gmail.com>
2024-11-08 22:07:46 -0500

    questions and answers

--- names.js
+++ names.js
@@ -1,13 +1,19 @@
 import inquirer from 'inquirer';
-import petNames from "./data/petNames"
+import petNames from './data/petNames'
+
+const questions = [
+  {type:"list", name:"petType", message:"What type of pet are you naming?", choices:["cat","dog"]}, 
+  {type:"list", name:"petGender", message:"What is your pet's gender?", choices:["female","male"]}, 
+  {type:"list", name:"nameType", message:"What type of name would you like?", choices:["cutesy","human"]}, 
+]
 
 inquirer
-  .prompt([
-    /* Pass your questions in here */
-  ])
-  .then((answers) => {
-    // Use user feedback for... whatever!!
+  .prompt(questions).then(answers => {
+  console.log(answers);
+  
+    
   })
+
   .catch((error) => {
     if (error.isTtyError) {
       // Prompt couldn't be rendered in the current environment
@@ -15,3 +21,5 @@ inquirer
       // Something else went wrong
     }
   });
+  
+  
